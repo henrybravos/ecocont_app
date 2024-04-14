d46b2b074e9659d1d3eca39c0501404fce5fc934
Henry Bravo <hbravos.info@gmail.com>
2024-04-14 08:50:52 -0500

    refactor: 💡 create barrel to services

---
+++ src/core/graphql/index.ts
@@ -0,0 +1,4 @@
+export { default as AuthService } from './AuthService'
+export { default as OrderSalesService } from './OrderSalesService'
+export { default as ProductService } from './ProductService'
+export { default as UserSalesService } from './UserSalesService'
